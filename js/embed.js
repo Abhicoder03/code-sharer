dfb8d03f069c182a8007b05a01298609c7d8523a
v <>
2020-09-21 21:14:54 -0400

    Text compression

--- js/embed.js
+++ js/embed.js
@@ -1,4 +1,6 @@
-const encoder = new TextDecoder()
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
 
 self.onmessage = function (e) {
 
--- js/share.js
+++ js/share.js
@@ -1,7 +1,14 @@
-const encoder = new TextEncoder()
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
 
 self.onmessage = function (e) {
-const view = encoder.encode(e.data)
+const view = str2ab(e.data)
   const link = view.join("+")
   console.log(self.location + "?embed=" + link)
 }
