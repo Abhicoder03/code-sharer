0dbf90a75f74a3e6b89a4708252c989836a17330
v <>
2020-09-24 14:22:44 -0400

    implementing itty bitty compression

--- js/share.js
+++ js/share.js
@@ -1,6 +1,12 @@
 importScripts("lzma-c.js");
 
-
+function stringToZip(string, callback) {
+  LZMA.compress(string, 9, function(result, error) {
+    if (error) console.error(error);
+    var base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(result)));
+    return callback(base64String);
+  });
+}
 
 
 
@@ -9,19 +15,17 @@ self.onmessage = function (e) {
 //the message here is code
 console.log(e.data)
 // to LZMA
-// LZMA.compress(e.data, 9, function(result, error) {
-//     if (error) console.error(error);
-//     self.lz = result
-//   });
+const lz = stringToZip(e.data)
+
 // to base64
-// console.log(typeof lz)
+console.log(typeof lz)
 // const conv = atob(lz)
 // construct url from base64
 
 // const link = self.location.origin + "#" + conv
 
-console.log(link)
-console.log(typeof link)
+// console.log(link)
+// console.log(typeof link)
 
 self.postMessage(link)
 
