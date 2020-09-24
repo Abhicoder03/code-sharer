7adeb0c5ddad4ad3e96bb01f30f5cc59fbfbd46b
v <>
2020-09-24 14:11:57 -0400

    Update share.js

--- js/share.js
+++ js/share.js
@@ -7,18 +7,18 @@ importScripts("lzma-c.js");
 self.onmessage = function (e) {
 
 //the message here is code
-
+console.log(e.data)
 // to LZMA
-LZMA.compress(e.data, 9, function(result, error) {
-    if (error) console.error(error);
-    self.lz = result
-  });
+// LZMA.compress(e.data, 9, function(result, error) {
+//     if (error) console.error(error);
+//     self.lz = result
+//   });
 // to base64
-console.log(typeof lz)
-const conv = atob(lz)
+// console.log(typeof lz)
+// const conv = atob(lz)
 // construct url from base64
 
-const link = self.location.origin + "#" + conv
+// const link = self.location.origin + "#" + conv
 
 console.log(link)
 console.log(typeof link)
