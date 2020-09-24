233726da567d1ff369b19166e95fd2789eaf11ef
v <>
2020-09-24 14:25:58 -0400

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,12 +1,6 @@
 importScripts("lzma-c.js");
 
-function stringToZip(string, callback) {
-  LZMA.compress(string, 9, function(result, error) {
-    if (error) console.error(error);
-    var base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(result)));
-    return callback(base64String);
-  });
-}
+
 
 
 
@@ -15,18 +9,22 @@ self.onmessage = function (e) {
 //the message here is code
 console.log(e.data)
 // to LZMA
-const lz = stringToZip(e.data)
-
+LZMA.compress(e.data, 9, function(result, error) {
+    if (error) console.error(error);
+    console.log(result)
+  });
 // to base64
-console.log(typeof lz)
+
+// console.log(typeof lz)
 // const conv = atob(lz)
+
 // construct url from base64
 
 // const link = self.location.origin + "#" + conv
-
+//
 // console.log(link)
 // console.log(typeof link)
 
-// self.postMessage(link)
+self.postMessage(link)
 
 }
