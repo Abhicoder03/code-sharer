3e179ef0eeb3664d60d9c8686165531bbe1c79b0
v <>
2020-09-24 13:53:26 -0400

    implement lzma

--- js/embed.js
+++ js/embed.js
@@ -1,7 +1,22 @@
-const encoder = new TextDecoder()
+importScripts("lzma-d.js");
 
 
 self.onmessage = function (e) {
+
+//the message here is a url hash
+
+console.log(e.data)
+console.log(typeof e.data)
+
+
+//deconstruct url
+const link = btoa(e.data)
+console.log(typeof link)
+
+LZMA.decompress(link, on_finish(result, error) {
+  console.log(result)
+  self.postMessage(result)
+});
 
 
 }
--- js/share.js
+++ js/share.js
@@ -1,10 +1,25 @@
-const encoder = new TextEncoder()
+importScripts("lzma-c.js");
+
 
 self.onmessage = function (e) {
-const view = encoder.encode(e.data)
-  const link = view.join("+")
-  console.log(self.location + "?embed=" + link)
-}
 
-//nah, just use assassin
-//oh no...I think I need to use free s3
+//the message here is code
+
+// to LZMA
+LZMA.compress(e.data, on_finish(result, error) {
+  console.log(result)
+  self.lz = result;
+});
+// to base64
+console.log(typeof lz)
+const conv = atob(lz)
+// construct url from base64
+
+const link = self.location.origin + "#" + conv
+
+console.log(link)
+console.log(typeof link)
+
+self.postMessage(link)
+
+}
