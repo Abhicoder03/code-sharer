0d2be48b01e1b0b6e828234bd4e120bc94e32bc9
v <>
2020-09-24 18:59:21 -0400

    understanding decompression steps

--- js/embed.js
+++ js/embed.js
@@ -1,5 +1,39 @@
 importScripts("lzma-d.js");
 
+var BASE64_MARKER = ';base64,';
+var LZMA64_MARKER = ';bxze64,';
+
+function zipToString(data, callback) {
+  var array = base64ToByteArray(data);
+  LZMA.decompress(array, function(result, error) {
+    if (!(typeof result === 'string')) result = new Uint8Array(result)
+    if (error) console.error(error);
+    callback(result);
+  });
+}
+
+function stringToData(string, callback) {
+  if (!string.length) return callback("");
+  var a = new FileReader();
+  a.onload = function(e) { callback(e.target.result.replace()) }
+  a.readAsDataURL(new Blob([string], {encoding:"UTF-8",type:"text/html;charset=UTF-8"}));
+}
+
+function decompressDataURI(dataURI, preamble, callback) {
+  var base64Index = dataURI.indexOf(LZMA64_MARKER);
+  if (base64Index > 0) {
+    var base64 = dataURI.substring(base64Index + LZMA64_MARKER.length);
+    zipToString(base64, function(result) {
+      stringToData(result, function(data) {
+        if (!data) return callback(undefined);
+        self.decoded = (dataURI.substring(0, base64Index) + BASE64_MARKER + (preamble || '') + data.split(',')[1])
+      })
+    })
+  } else {
+    callback(dataURI)
+  }
+}
+
 function base64ToByteArray(base64) {
   var raw = atob(base64);
   var rawLength = raw.length;
@@ -14,25 +48,16 @@ self.onmessage = function (e) {
 
 //the message here is a url hash
 
-const array = base64ToByteArray(e.data);
+
+decompressDataURI(e.data);
+const array = base64ToByteArray(decoded)
+
  LZMA.decompress(array, function(result, error) {
    if (!(typeof result === 'string')) result = new Uint8Array(result)
    if (error) console.error(error);
    console.log("Decoded result is:" + result)
  });
 
-// console.log(e.data)
-// console.log(typeof e.data)
-//
-//
-// //deconstruct url
-// const link = btoa(e.data)
-// console.log(typeof link)
-//
-// LZMA.decompress(link, 9, function(result, error) {
-//     if (error) console.error(error);
-//     console.log(result)
-//     self.postMessage(result)
-//   });
+
 
 }
