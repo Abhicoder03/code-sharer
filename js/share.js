08cca9a4cc54b999a7e308e5a987e8cef134db5b
v <>
2020-09-24 18:37:58 -0400

    Update share.js

--- js/share.js
+++ js/share.js
@@ -10,7 +10,8 @@ console.log(e.data)
   LZMA.compress(e.data, 9, function(result, error) {
     if (error) console.error(error);
     const base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(result)));
-    self.postMessage(self.location.origin + "#" + base64String)
+    self.postMessage(self.location.origin + "#" + base64String);
+    console.log(base64String)
   });
 
 
