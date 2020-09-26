a43d5497a7a2666bae309758bcf6a26d2e3dcc8a
v <>
2020-09-26 16:36:31 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,6 +11,44 @@ function clip() {
   var tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copied!"
 }
+=======
+HTMLTextAreaElement.prototype.getCaretPosition = function () { //return the caret position of the textarea
+    return this.selectionStart;
+};
+HTMLTextAreaElement.prototype.setCaretPosition = function (position) { //change the caret position of the textarea
+    this.selectionStart = position;
+    this.selectionEnd = position;
+    this.focus();
+};
+HTMLTextAreaElement.prototype.hasSelection = function () { //if the textarea has selection then return true
+    if (this.selectionStart == this.selectionEnd) {
+        return false;
+    } else {
+        return true;
+    }
+};
+HTMLTextAreaElement.prototype.getSelectedText = function () { //return the selection text
+    return this.value.substring(this.selectionStart, this.selectionEnd);
+};
+HTMLTextAreaElement.prototype.setSelection = function (start, end) { //change the selection area of the textarea
+    this.selectionStart = start;
+    this.selectionEnd = end;
+    this.focus();
+};
+
+text.onkeydown = function(event) {
+
+    //support tab on textarea
+    if (event.keyCode == 9) { //tab was pressed
+        var newCaretPosition;
+        newCaretPosition = text.getCaretPosition() + "    ".length;
+        text.value = text.value.substring(0, text.getCaretPosition()) + "    " + text.value.substring(text.getCaretPosition(), text.value.length);
+        text.setCaretPosition(newCaretPosition);
+        return false;
+    }
+  }
+
+>>>>>>> Stashed changes
 
 window.addEventListener('load', (event) => {
 
