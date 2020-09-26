451e5d0a5c9a95c27c32480213a48c8ce31ee1ef
v <>
2020-09-26 19:56:53 -0400

    Update embed.js

--- js/embed.js
+++ js/embed.js
@@ -12,19 +12,6 @@ function zipToString(data) {
 }
 
 
-function decompressDataURI(dataURI) {
-  var base64Index = dataURI.indexOf(LZMA64_MARKER);
-  if (base64Index > 0) {
-    var base64 = dataURI.substring(base64Index + LZMA64_MARKER.length);
-    console.log("if: ", base64)
-    // zipToString(base64, function(result) {
-    //     self.decoded = (dataURI.substring(0, base64Index) + BASE64_MARKER + '' + data.split(',')[1])
-    // })
-  } else {
-    console.log("else: ", base64)
-  }
-}
-
 function base64ToByteArray(base64) {
   var raw = atob(base64);
   var rawLength = raw.length;
@@ -40,14 +27,13 @@ self.onmessage = function (e) {
 //the message here is a url hash
 
 
-decompressDataURI(e.data);
-// const array = base64ToByteArray(decoded)
-//
-//  LZMA.decompress(array, function(result, error) {
-//    if (!(typeof result === 'string')) result = new Uint8Array(result)
-//    if (error) console.error(error);
-//    console.log("Decoded result is:" + result)
-//  });
+const array = base64ToByteArray(e.data)
+
+ LZMA.decompress(array, function(result, error) {
+   if (!(typeof result === 'string')) result = new Uint8Array(result)
+   if (error) console.error(error);
+   console.log("Decoded result is:" + result)
+ });
 
 
 
