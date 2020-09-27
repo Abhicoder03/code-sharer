0e2b7007a384f286a142ab73219da72281a40861
Ender Minyard <60559370+genderev@users.noreply.github.com>
2020-09-27 01:18:59 +0000

    Merging is hard
    Why is merging hard

--- js/embed.js
+++ js/embed.js
@@ -1,38 +1,5 @@
 importScripts("lzma-d.js");
 
-var BASE64_MARKER = ';base64,';
-var LZMA64_MARKER = ';bxze64,';
-
-function zipToString(data, callback) {
-  var array = base64ToByteArray(data);
-  LZMA.decompress(array, function(result, error) {
-    if (!(typeof result === 'string')) result = new Uint8Array(result)
-    if (error) console.error(error);
-    callback(result);
-  });
-}
-
-function stringToData(string, callback) {
-  if (!string.length) return callback("");
-  var a = new FileReader();
-  a.onload = function(e) { callback(e.target.result.replace()) }
-  a.readAsDataURL(new Blob([string], {encoding:"UTF-8",type:"text/html;charset=UTF-8"}));
-}
-
-function decompressDataURI(dataURI, preamble, callback) {
-  var base64Index = dataURI.indexOf(LZMA64_MARKER);
-  if (base64Index > 0) {
-    var base64 = dataURI.substring(base64Index + LZMA64_MARKER.length);
-    zipToString(base64, function(result) {
-      stringToData(result, function(data) {
-        if (!data) return callback(undefined);
-        self.decoded = (dataURI.substring(0, base64Index) + BASE64_MARKER + (preamble || '') + data.split(',')[1])
-      })
-    })
-  } else {
-    callback(dataURI)
-  }
-}
 
 function base64ToByteArray(base64) {
   var raw = atob(base64);
@@ -49,13 +16,13 @@ self.onmessage = function (e) {
 //the message here is a url hash
 
 
-decompressDataURI(e.data);
-const array = base64ToByteArray(decoded)
+const array = base64ToByteArray(e.data.replace("#",""))
 
  LZMA.decompress(array, function(result, error) {
-   if (!(typeof result === 'string')) result = new Uint8Array(result)
+   if (!(typeof result === 'string')) result = new Uint8Array(result);
    if (error) console.error(error);
-   console.log("Decoded result is:" + result)
+   console.log("Decoded result is:" + result);
+   self.postMessage(result);
  });
 
 
--- js/share.js
+++ js/share.js
@@ -10,22 +10,9 @@ console.log(e.data)
   LZMA.compress(e.data, 9, function(result, error) {
     if (error) console.error(error);
     const base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(result)));
-    self.postMessage(self.location.origin + "#" + base64String);
-    console.log(base64String)
+    self.postMessage(self.location.origin + "/#" + base64String);
+    console.log(base64String);
   });
 
-
-
-// to base64
-// const conv = atob(lz)
-
-// construct url from base64
-
-// const link = self.location.origin + "#" + conv
-//
-// console.log(link)
-// console.log(typeof link)
-
-// self.postMessage(link)
 
 }
