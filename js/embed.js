065ea07585ffc46e02e8f56a694b987b0c3ac83d
v <>
2020-09-26 20:00:11 -0400

    I think I just figured out decompression

--- js/embed.js
+++ js/embed.js
@@ -1,16 +1,5 @@
 importScripts("lzma-d.js");
 
-var BASE64_MARKER = ';base64,';
-var LZMA64_MARKER = ';bxze64,';
-
-function zipToString(data) {
-  var array = base64ToByteArray(data);
-  LZMA.decompress(array, function(result, error) {
-    if (!(typeof result === 'string')) result = new Uint8Array(result)
-    if (error) console.error(error);
-  });
-}
-
 
 function base64ToByteArray(base64) {
   var raw = atob(base64);
@@ -30,9 +19,10 @@ self.onmessage = function (e) {
 const array = base64ToByteArray(e.data.replace("#",""))
 
  LZMA.decompress(array, function(result, error) {
-   if (!(typeof result === 'string')) result = new Uint8Array(result)
+   if (!(typeof result === 'string')) result = new Uint8Array(result);
    if (error) console.error(error);
-   console.log("Decoded result is:" + result)
+   console.log("Decoded result is:" + result);
+   self.postMessage(result)
  });
 
 
